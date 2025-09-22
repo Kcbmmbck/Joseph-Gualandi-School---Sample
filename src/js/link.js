9bc58b9552a768ac1706369c9884c2e42c139cba
Kean Christopher Moscoso <kcbm65@gmail.com>
2025-09-22 17:10:49 +0800

    Update link.js

--- src/js/link.js
+++ src/js/link.js
@@ -1,7 +1,8 @@
 document.getElementById("button1").addEventListener("click", function () {
-  window.location.href = "/JGHS-Las-Pinas/Joseph-Gualandi-School-Las-Piñas.html";
+  window.location.href = "JGHS-Las-Pinas/Joseph-Gualandi-School-Las-Piñas.html";
 });
 
 document.getElementById("button2").addEventListener("click", function () {
-  window.location.href = "/JGHS-Naga/Joseph-Gualandi-School-Naga.html";
+  window.location.href = "JGHS-Naga/Joseph-Gualandi-School-Naga.html";
+
 });
